8b3dca7d29f1613eeb6f9700687a98c28f0ee490
Marion Rose <marionelizabethrose@gmail.com>
2018-01-09 21:43:13 -0600

    form testing

--- src/components/contact-content.js
+++ src/components/contact-content.js
@@ -12,6 +12,35 @@ export default class Index extends React.Component {
         <h2>Marion Rose</h2>
         <p>Graphic Designer & Budding Web Developer</p>
 
+				<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+				      <input type="text" name="name" />
+				      <input type="email" name="email" />
+				      <textarea name="message"></textarea>
+				    </form>
+
+				    <div id="root"></div>
+				    <script type="text/babel">
+
+				      ReactDOM.render(
+				        <form method="post">
+				          <input type="hidden" name="form-name" value="contact" />
+				          <p>
+				            <label>Your Name: <input type="text" name="name"/></label>
+				          </p>
+				          <p>
+				            <label>Your Email: <input type="email" name="email"/></label>
+				          </p>
+				          <p>
+				            <label>Message: <textarea name="message"></textarea></label>
+				          </p>
+				          <p>
+				            <button type="submit">Send</button>
+				          </p>
+				        </form>,
+				        document.getElementById("root")
+				      );
+
+				    </script>
 
 
 
