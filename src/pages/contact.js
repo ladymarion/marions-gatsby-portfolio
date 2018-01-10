3643dec96a4f011fadf4af0291e11850f43d9e00
Marion Rose <marionelizabethrose@gmail.com>
2018-01-09 21:56:06 -0600

    working w forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,30 +12,21 @@ export default class Index extends React.Component {
         <h2>Marion Rose</h2>
         <p>Graphic Designer & Budding Web Developer</p>
 
-				<form name="contact" netlify netlify-honeypot="bot-field" hidden>
-				      <input type="text" name="name" />
-				      <input type="email" name="email" />
-				      <textarea name="message"></textarea>
-				    </form>
+				<form name="contact" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name"/></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email"/></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
-				    <div id="root"></div>
-
-				        <form method="post">
-				          <input type="hidden" name="form-name" value="contact" />
-				          <p>
-				            <label>Your Name: <input type="text" name="name"/></label>
-				          </p>
-				          <p>
-				            <label>Your Email: <input type="email" name="email"/></label>
-				          </p>
-				          <p>
-				            <label>Message: <textarea name="message"></textarea></label>
-				          </p>
-				          <p>
-				            <button type="submit">Send</button>
-				          </p>
-				        </form>,
-				   
 
 
 
